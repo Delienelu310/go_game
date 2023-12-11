7c46c52c5ece09107cc67125f773d4c97a66608b
Delienelu310 <seredoha.nets@gmail.com>
2023-12-11 13:04:39 +0100

    Initialize project: spring boot app and react app

---
+++ go_game_backend/src/main/java/com/teoriaprogramowania/go_game/GoGameApplication.java
@@ -0,0 +1,13 @@
+package com.teoriaprogramowania.go_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoGameApplication.class, args);
+	}
+
+}
---
+++ go_game_backend/src/test/java/com/teoriaprogramowania/go_game/GoGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teoriaprogramowania.go_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
